12aceefd08cbbbbe7c5b1ae14fc0bfb64ad8d7a2
Utsukara <160966817+Utsukara@users.noreply.github.com>
2024-04-08 23:26:15 -0500

    Create Task3_1.py

---
+++ Task3_1.py
@@ -0,0 +1,4 @@
+# Task 1: Public Transportation Module
+
+# Problem Statement: Develop a Bus class as part of a public transportation module. Use class variables to represent common attributes like city name and base fare. Implement instance variables for specific attributes like route number and passenger capacity.
+# Expected Outcome: A Bus class with both class and instance variables, and a transportation module to manage different bus routes. A test script should demonstrate creating bus instances and accessing both class and instance attributes.
